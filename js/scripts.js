5fae2cd985e0780b4e5a8803dd51cad57d8c47ec
Claire Sial and Suzi Rubino <claire@>
2016-10-26 09:24:03 -0700

    create initial files for leap year calculator

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+  $("form#leapYear").submit(function(event){
+
+    var userYear = $("input#birthYear").val();
+    
+
+    event.preventDefault();
+
+  });
+
+
+
+});
